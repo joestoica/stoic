d2f4eb153ee883e72d2691c873e0d3911081d8de
joestoica <joe.stoica@gmail.com>
2019-03-26 14:39:59 -0400

    updated subtitle and caption for ggplot theme

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,4 +1,4 @@
-# ggplot2 theme
+ # ggplot2 theme
 theme_stoic <- function(){
 
     theme_bw(base_size=12, base_family="sans") %+replace%
@@ -15,9 +15,30 @@ theme_stoic <- function(){
               panel.grid = element_line(colour = NULL),
               panel.grid.major = element_line(colour = "#D2D2D2"),
               panel.grid.minor = element_blank(),
-              plot.caption = element_text(color = "#9B9B9B"),
               plot.margin = unit(c(1,1, 1, 1), "lines"),
-              plot.subtitle = element_text(size = 14),
               plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
               strip.background = element_rect())
 }
+
+# Fivethirtyeight
+# function (base_size = 12, base_family = "sans")
+# {
+#     (theme_foundation(base_size = base_size, base_family = base_family) +
+#          theme(line = element_line(colour = "black"),
+#                rect = element_rect(fill = ggthemes_data$fivethirtyeight["ltgray"], linetype = 0, colour = NA),
+#                text = element_text(colour = ggthemes_data$fivethirtyeight["dkgray"]),
+#                axis.title = element_blank(),
+#                axis.text = element_text(),
+#                axis.ticks = element_blank(),
+#                axis.line = element_blank(),
+#                legend.background = element_rect(),
+#                legend.position = "bottom",
+#                legend.direction = "horizontal",
+#                legend.box = "vertical",
+#                panel.grid = element_line(colour = NULL),
+#                panel.grid.major = element_line(colour = ggthemes_data$fivethirtyeight["medgray"]),
+#                panel.grid.minor = element_blank(),
+#                plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
+#                plot.margin = unit(c(1, 1, 1, 1), "lines"),
+#                strip.background = element_rect()))
+# }
